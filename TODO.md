913860986685e1b2a56779a097892ba7b53b0cfa
沈 晓雷 <819452727@qq.com>
2026-02-06 03:57:01 +0800

    修复神秘deathflood死亡信息会多次发送

--- TODO.md
+++ TODO.md
@@ -1,2 +1,2 @@
-deathflood死亡信息会多次发送
+deathflood死亡信息会多次发送(草,还就只有deathflood死会多次发信,为啥呢?即死有特殊判定?)
 多模型加载
--- src/Core/Patch/Patch_ENT_Player.cs
+++ src/Core/Patch/Patch_ENT_Player.cs
@@ -7,10 +7,16 @@ namespace WKMPMod.Patch;
 
 [HarmonyPatch(typeof(ENT_Player))]
 public class Patch_ENT_Player {
-	[HarmonyPostfix]
+	[HarmonyPrefix]
 	[HarmonyPatch("Kill")]
-	public static void Postfix(ENT_Player __instance, string type) {
-		if (MPCore.IsInLobby) {
+	public static void Prefix(ENT_Player __instance, string type) {
+		// 死亡切换发生前通知总线
+		// 避免死亡后重复通知
+
+		// Debug
+		//MPMain.LogInfo($"[Patch] 玩家状态 {__instance.dead}");
+
+		if (MPCore.IsInLobby&& __instance.dead == false) {
 			MPEventBusGame.NotifyPlayerDeath(type);
 			MPMain.LogInfo(Localization.Get("Patch", "PlayerDeath", type));
 		}
